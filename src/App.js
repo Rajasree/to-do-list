8e0dc7fa78bdd7645a8c99737e8dffc9edac4073
Neethu Sadasivan <neethusadasivan8@gmail.com>
2021-05-25 09:55:24 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import KeyBoard from './KeyBoard'
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>ToDo List</h1>
+      </div>
+      <div>
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type='text' placeholder='Add item...'></input>
+        <i onClick={()=>setToDos([...toDos,{id: Date.now(),text: toDo, status: false}])} className='fas fa-plus'></i>
+      </div>
+      <h2>Created List</h2>
+      { toDos.map((objct) => {
+        return(
+      <div id={objct.id}>
+        <div>
+          <input onChange={(e)=>setToDos(toDos.filter(obj2=>{
+            if(obj2.id===objct.id){
+              obj2.status = e.target.checked
+            }
+            return obj2
+          }))} value={objct.status} type='checkbox' name='' id=''></input>
+          <p>{objct.text}</p>
+        </div>
+        <div>
+        <i onClick={()=>setToDos(toDos.filter(obj2=>{
+          if(obj2.id===objct.id){
+            obj2.status = false
+            document.getElementById(obj2.id).remove()
+          }
+          return obj2
+        }))} className="fas fa-times"></i>
+        </div>
+      </div>)})
+      }
+      <h3>Selected Items</h3>
+      <div>
+        {toDos.map((ob)=>{
+          if(ob.status){
+            return(<p>{ob.text}</p>)
+          }
+          return null
+        })}
+      </div>
     </div>
+    
+
   );
 }
 
---
+++ src/KeyBoard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class KeyBoard extends React.Component {
+ render(){
+     return(
+         <div>
+             <div>
+                 <button name="1"></button>
+             </div>
+         </div>
+     )
+ }   
+}
